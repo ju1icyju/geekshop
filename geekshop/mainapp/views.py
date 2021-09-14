2ea1da9045fd54f2c2eca05a0e2d1da955d83339
ju1icyju <toxa.tomkovich@gmail.com>
2021-09-14 21:32:09 +0300

    3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from mainapp.models import Product
+
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = 'Главная'
+    products = Product.objects.all()
+    content = {'title': title, 'products': products}
+    return render(request, 'mainapp/index.html', content)
 
 def products(request):
     return render(request, 'mainapp/products.html')
